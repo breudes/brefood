516791b41b88aee2ff8e8abb734b709ca48d2ca4
Brenda Souza <breudes@outlook.com>
2024-09-16 21:50:21 -0300

    Gateway added

--- gateway/src/test/java/com/brefood/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/com/brefood/gateway/GatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.brefood.pedidos;
+package com.brefood.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PedidosApplicationTests {
+class GatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
